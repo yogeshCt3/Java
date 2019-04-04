bf1a86bbba78108b2e7444046e42e645c8c498f5
Yogesh Chhetri <yogeshkchhetri@gmail.com>
2019-04-04 20:46:58 +0530

    Create 04 - coumpound operator.java

---
+++ 04 - coumpound operator.java
@@ -0,0 +1,11 @@
+class FirstCode{	
+	public static void main(String [] YC){
+		char k = 65;
+		k += 1;
+		// k = k + 1; error
+//		int l = (3.5 / 2);	error
+		byte c = 10, d =20;
+//		byte ln = c + d; 	error
+		System.out.println();
+	}
+}
