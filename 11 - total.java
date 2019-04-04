129869ce0b9d63a610ec8b233409a91719d10ef4
Yogesh Chhetri <yogeshkchhetri@gmail.com>
2019-04-04 20:47:06 +0530

    Create 11 - total.java

---
+++ 11 - total.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Topper{
+	static int total_subject, ;
+	
+	public static void main(String ... Dj){
+		Scanner cin = new Scanner(System.in);
+		System.out.print("Enter numbert of marks : ");
+		int n = cin.nextInt();
+		
+	}
+}
